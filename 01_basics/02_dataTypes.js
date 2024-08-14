12b7400e1a30e0a910b51da95102184ef4ec9fe7
toru-06 <165811556+toru-06@users.noreply.github.com>
2024-08-14 19:16:35 +0000

    dtattypes and theircoversion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+// alert(3+3)  // we are using nodejs,not browser
+
+let name = "Tarun";              // string
+let age = 21;                    // number
+let isLoggedIN = false;          // boolean
+
+// number 
+// bigint
+// boolean
+// string => ""
+// null   => standalone value
+// undefined 
+// symbol  => unique
+
+// object
+
+console.log(typeof name);      // string
+console.log(typeof null);      // null is an object 
+console.log(typeof undefined); // undefined
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33ab";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let scoreInNumber = Number(score);
+//console.log(typeof scoreInNumber);
+//console.log(scoreInNumber);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1 ; false => 0;
+
+let isLoggedIN = "";
+let booleanISLoggedIN = Boolean(isLoggedIN);
+//console.log(booleanISLoggedIN);
+//console.log(typeof booleanISLoggedIN);
+
+// "" => false;
+// "hsa" => true;
+// 1 => true;
+// 0 => false;
+
+let  someNumber = 5;
+let  stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
