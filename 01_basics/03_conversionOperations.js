b070019473c336d95eb5ed7bda6420042cbb3d13
toru-06 <165811556+toru-06@users.noreply.github.com>
2024-08-18 11:01:00 +0000

    summary

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,5 +23,46 @@ let booleanISLoggedIN = Boolean(isLoggedIN);
 
 let  someNumber = 5;
 let  stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " tarun";
+
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(2 +  "1");
+// console.log("1" + 2 +3);
+// console.log(1+1+"2");
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(false);
+// console.log(+false);
+// console.log(+"");
+
+
+// let count = 4;
+// let newValue = count++;
+// console.log(count);
+// console.log(newValue);
+
+let count = 4;
+let newValue = ++count;
+// console.log(count);
+// console.log(newValue);
+
